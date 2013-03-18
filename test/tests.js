e57849bc121d91e46b3adee7976e36216152fd6c
TJ Holowaychuk <tj@vision-media.ca>
2013-03-18 09:23:29 -0700

    adjust test

--- test/tests.js
+++ test/tests.js
@@ -5,12 +5,15 @@ var toBlob = require('data-uri-to-blob');
 var canvas = document.createElement('canvas');
 canvas.width = 5;
 canvas.height = 5;
+var ctx = canvas.getContext('2d');
+ctx.fillRect(0, 0, 5, 5);
 
 describe('blob(uri)', function(){
   it('should return a Blob', function(){
     var uri = canvas.toDataURL();
     var blob = toBlob(uri);
+    var type = {}.toString.call(blob);
+    assert('[object Blob]' == type);
     assert('image/png' == blob.type, 'invalid .type');
-    assert(72 == blob.size, 'invalid .size');
   })
 })
